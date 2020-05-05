57cd30305203fc717c809dac9b3d89515e87351b
koraykural <koraykural99@gmail.com>
2020-05-05 18:47:39 +0300

    process env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 let pool;
 pool = new Pool({
-  connectionString: 'postgres://rfejkuxfgnapqc:7e58fd3e27d75c9a5c3bd801d4ec38bc5b196748f8cbcca926f71a0df5d2926d@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d9n39tt5ivnh13',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
