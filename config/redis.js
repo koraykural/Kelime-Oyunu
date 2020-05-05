657be14da5359a638489c2c6d4edac0843dcb53f
koraykural <koraykural99@gmail.com>
2020-05-05 22:56:13 +0300

    remove dev urls

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
-const redisUrl = process.env.REDIS_URL || 'redis://h:p33ab237b3b0f190bf3c083386886e34b67eb6bc870b6f3cd8704cefdae3fb008@ec2-52-31-65-39.eu-west-1.compute.amazonaws.com:25589';
+const redisUrl = process.env.REDIS_URL;
 const client = redis.createClient(redisUrl);
 const { promisify } = require('util');
 client.SRANDMEMBER = promisify(client.SRANDMEMBER); 
